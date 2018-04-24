d81c83f6c49f1649dfde43c122d90302efffea16
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-04-24 15:26:39 -0400

    added revisions from another pc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
     //VARIABLES
    
-    var words=["MCMUFFIN","MCNUGGETS","MCGRIDDLE","BIGMAC","FILETOFISH"]
+    var words=["MCMUFFIN","MCNUGGETS","MCGRIDDLE","BIGMAC","FILETOFISH","MCFRIES"]
+    var images=["./assets/images/mcmuffin.png","./assets/images/mcnuggets.png","./assets/images/mcgriddle.png","./assets/images/bigmac.png","./assets/images/filetofish.png","./assets/images/mcfries.png","./assets/images/loss3.png"]
     var wins = 0;
     var losses = 0;
     var guesses = 10;
@@ -15,6 +16,7 @@
     //FUNCTIONS
 
     //Reset
+
     function reset() {
         wait(200);
         runPCGuess();
@@ -24,6 +26,27 @@
         document.getElementById("used-letters").innerHTML = "Used Letters: ";
         document.getElementById('remaining-guesses').innerHTML= "Remaining guesses: " + remainingGuess; 
     };
+
+    //Update image
+    function updateImage() {
+        if (pcGuess == words[0]) {
+            document.getElementById('picture').src=images[0]; 
+        } else  if (pcGuess == words[1]) {
+            document.getElementById('picture').src=images[1]; 
+        }else  if (pcGuess == words[2]) {
+            document.getElementById('picture').src=images[2]; 
+        }else  if (pcGuess == words[3]) {
+            document.getElementById('picture').src=images[3]; 
+        }else  if (pcGuess == words[4]) {
+            document.getElementById('picture').src=images[4]; 
+        }else  if (pcGuess == words[5]) {
+            document.getElementById('picture').src=images[5]; 
+    }}
+
+    function sadImage() {
+        document.getElementById('picture').src=images[6]; 
+    }
+
 
     //Wait
     function wait(ms){
@@ -63,6 +86,7 @@
         document.getElementById("current-word").innerHTML = pcGuess;
         document.getElementById('wins').innerHTML = "Wins: " + wins;
         document.getElementById('old-word').innerHTML = pcGuess;
+        updateImage();
         reset();
     };
     
@@ -71,6 +95,7 @@
         losses++;
         document.getElementById('losses').innerHTML = "Losses: " + losses;
         document.getElementById('old-word').innerHTML = "";
+        sadImage();
         reset();
     };
     
